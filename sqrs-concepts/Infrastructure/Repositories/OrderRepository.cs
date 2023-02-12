befa64934dd582a79f4891e5e8a894b6c6f1b4e3
Alphanon <aliroshan@live.com>
2023-02-12 19:50:31 +0500

    Added docker support

---
+++ sqrs-concepts/Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using sqrs_concepts.Infrastructure.Streams;
+
+namespace sqrs_concepts.Infrastructure.Repositories
+{
+	public class OrderRepository : BaseRepository
+	{
+		public OrderRepository(IEventWriter eventWriter)
+			: base(eventWriter)
+		{
+		}
+	}
+}
